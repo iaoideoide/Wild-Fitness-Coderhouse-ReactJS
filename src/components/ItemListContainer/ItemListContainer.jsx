2533e61c5b9250da7b775572c089c24a9e004242
Iao <iairbarak@gmail.com>
2023-02-16 14:22:45 -0300

    actualizado primer preentrega

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./itemListContainer.css";
+
+const ItemListContainer = () => {
+  return (
+    <div className="ItemListContainer">
+      <h1>WILD FITNESS</h1>
+    </div>
+  );
+};
+
+export default ItemListContainer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import { BsFillCartFill } from "react-icons/bs";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links_logo">
+          <img src={logo} />
+        </div>
+        <div className="navbar-links_container">
+          <p>
+            <a href="#">Inicio</a>
+          </p>
+          <p>
+            <a href="#">Accesorios</a>
+          </p>
+          <p>
+            <a href="#">Equipamento</a>
+          </p>
+          <p>
+            <a href="#">Suplementos</a>
+          </p>
+        </div>
+        <div className="navbar-cart">
+          <BsFillCartFill color="#e31515" size={24} />
+          <span className="navbar-cart__cant">3</span>
+        </div>
+      </div>
+
+      <div className="navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar-menu_container scale-up-center">
+            <div className="navbar-menu_container-links">
+              <p>
+                <a href="#">Inicio</a>
+              </p>
+              <p>
+                <a href="#">Accesorios</a>
+              </p>
+              <p>
+                <a href="#">Equipamento</a>
+              </p>
+              <p>
+                <a href="#">Suplementos</a>
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
