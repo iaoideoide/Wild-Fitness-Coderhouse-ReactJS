13629baf32e429722a08284c55af448e60960ab9
Iao <iairbarak@gmail.com>
2023-02-27 17:01:18 -0300

    se agrego el contador

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Counter from "./components/Counter/Counter";
 import "./App.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <Navbar />
       <ItemListContainer />
+      <Counter />
     </div>
   );
 };
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import "./counter.css";
+import { IoIosRemoveCircleOutline } from "react-icons/io";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { IoTrashSharp } from "react-icons/io5";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter">
+      <button className="counter__btn" onClick={decreaseCount}>
+        <IoIosRemoveCircleOutline color="#000" size={24} />
+      </button>
+      <h1>{count}</h1>
+      <button className="counter__btn" onClick={increaseCount}>
+        <IoIosAddCircleOutline color="#000" size={24} />
+      </button>
+      <button className="counter__btn" onClick={resetCount}>
+        <IoTrashSharp color="#000" size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
